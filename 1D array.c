f1b5cbbe553b884aed8f8785eae47908a594a4a3
Cosmas-pro <91739410+Cosmas-pro@users.noreply.github.com>
2021-11-18 14:39:07 +0300

    Create 1D array.c

---
+++ 1D array.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main ( ){
+   int a[5],i;
+   printf ("enter 5 elements");
+   for ( i=0; i<5; i++)
+      scanf("%d", &a[i]);
+   printf("elements of the array are");
+   for (i=0; i<5; i++)
+      printf("%d", a[i]);
+}
